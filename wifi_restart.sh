6cae0afa86ade549a10a167ba87d2f32ade80f29
Andrej Hosna <hosna.andrej@gmail.com>
2021-02-04 13:48:14 +0000

    keep wifi up script

--- wifi_restart.sh
+++ wifi_restart.sh
@@ -2,17 +2,18 @@
 
 ROUTER="192.168.1.1"
 INTERFACE="wlan0"
-INTERVAL=1
+INTERVAL=60
 
 
 while /bin/sleep $INTERVAL; do
-    #/sbin/ping "$ROUTER" -c 10 -i 0.5 -W 0.5 | grep "bytes from" > /dev/null 2>&1  # OSX
-    /sbin/ping "$ROUTER" -i 0.5 -w 5 | grep "bytes from" > /dev/null 2>&1
-    if [ $? -eq 0 ]; then
+    #/sbin/ping "$ROUTER" -c 10 -i 0.5 -W 0.5 | /bin/grep "bytes from" > /dev/null 2>&1  # OSX
+    /bin/ping "$ROUTER" -i 0.5 -w 5 | /bin/grep "bytes from" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
         /usr/bin/logger "Can't ping the uplink $ROUTER. Restarting $INTERFACE"
         /sbin/ifdown $INTERFACE
         /bin/sleep 5
         /sbin/ifup $INTERFACE
+	/usr/bin/logger "Restart complete"
     else
         /usr/bin/logger "Router is pingable. Everything is fine."
     fi
