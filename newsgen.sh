901a8366a267526f1ce42702fcaa0622a8021927
Andrej Hosna <adino@uid0.sk>
2021-02-09 21:40:36 +0100

    We need python 3.7

--- newsgen.sh
+++ newsgen.sh
@@ -4,8 +4,5 @@ MY_PATH=/home/adino/code/Pi0AudioBook
 . $MY_PATH/env/bin/activate
 export GOOGLE_APPLICATION_CREDENTIALS="$MY_PATH/env/newsgen-credentials.json"
 cd $MY_PATH
-python3.7 ./newsgen.py
-
-scp /tmp/.news.mp3 kniha:/data/.news.mp3
-
+python3.7 ./newsgen.py 
 exit $?
