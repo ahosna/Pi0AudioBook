12785798a76775f156eb61f251f6e4775e31524e
Andrej Hosna <adino@uid0.sk>
2021-09-28 19:39:41 +0200

    script to prebuild clock announcement for every minute of the day

---
+++ prebuildtime.py
@@ -0,0 +1,53 @@
+import datetime
+import os
+import json
+import logging
+from google.cloud import texttospeech
+
+
+def wrap_in_p(lines):
+    return ["<p>{}</p>".format(l) for l in lines]
+
+
+
+def gen_time(hours, minutes):
+    logging.info("Calling google API")
+    try:
+        client = texttospeech.TextToSpeechClient()
+
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="sk-SK",
+            name="sk-SK-Wavenet-A",
+        )
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3,
+            speaking_rate=0.75,
+            pitch=-1.0,
+        )
+        ssml = (
+            "<speak>"
+            + "<p>Práve je {} hodín a {} minút</p>".format(hours, minutes)
+            + "</speak>"
+        )
+
+        synthesis_input = texttospeech.SynthesisInput(ssml=ssml)
+
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+
+        filename = "times/%02dh-%02dm.mp3" % (hours, minutes, )
+        logging.info(f"Filename: {filename}")
+        with open(filename, "wb") as out:
+            out.write(response.audio_content)
+            logging.info("Audio content written to file")
+
+    except Exception as e:
+        logging.exception(e)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    for hours in range(24):
+        for minutes in range(60):
+            gen_time(hours, minutes)
