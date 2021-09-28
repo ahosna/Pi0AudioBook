e21c8b6da192b7e0c9af7ba568ac11df80503f2a
Andrej Hosna <hosna.andrej@gmail.com>
2021-09-28 19:09:48 +0100

    new helper to merge time and announcement

---
+++ time_and_newsgen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+NEWS_FILE="/data/.news.mp3"
+ANNOUNCER_FILE="/tmp/.anziko.mp3"
+
+if [ ! -f $NEWS_FILE ]; then
+    exit 0
+fi
+
+tf=`date +times/%0Hh-%0Mm.mp3`
+ffmpeg -i "concat:$tf|$NEWS_FILE" -c copy $ANNOUNCER_FILE
+exit 0
