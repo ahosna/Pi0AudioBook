044665f04d9c204e57ae30a7437e4cf55d0b30a1
Andrej Hosna <hosna.andrej@gmail.com>
2021-09-28 21:14:50 +0200

    Prevent mpd from crashing due to changes in the file that's being played

--- time_and_newsgen.sh
+++ time_and_newsgen.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-NEWS_FILE="/data/.news.mp3"
+NEWS_FILE="/data/tmp/.news.mp3"
+ANNOUNCER_FILE_TMP="/data/tmp/.tmpanziko.mp3"
 ANNOUNCER_FILE="/data/tmp/.anziko.mp3"
 
 if [ ! -f $NEWS_FILE ]; then
     exit 0
 fi
 
-tf=`date +times/%0Hh-%0Mm.mp3`
-ffmpeg -y -i "concat:$tf|$NEWS_FILE" -c copy $ANNOUNCER_FILE
+tf=`date +/home/pi/Pi0AudioBook/times/%0Hh-%0Mm.mp3`
+ffmpeg -y -i "concat:$tf|$NEWS_FILE" -c copy $ANNOUNCER_FILE_TMP
+mv -f $ANNOUNCER_FILE_TMP $ANNOUNCER_FILE
 exit 0
