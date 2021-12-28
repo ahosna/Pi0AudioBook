7b2eb9c237076a081dcb578e52f787bb09cbfbd6
Andrej Hosna <hosna.andrej@gmail.com>
2021-12-28 20:20:17 +0100

    Use startup message before announcing news/time.

--- time_and_newsgen.sh
+++ time_and_newsgen.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 NEWS_FILE="/data/tmp/.news.mp3"
+STARTUP_FILE="/home/pi/Pi0AudioBook/msgs/startup.mp3"
 ANNOUNCER_FILE_TMP="/data/tmp/.tmpanziko.mp3"
 ANNOUNCER_FILE="/data/tmp/.anziko.mp3"
 
-tf=`date +/home/pi/Pi0AudioBook/times/%0Hh-%0Mm.mp3`
 if [ ! -f $NEWS_FILE ]; then
-    cp $tf $ANNOUNCER_FILE_TMP
+    cp $STARTUP_FILE $ANNOUNCER_FILE_TMP
 else
+    tf=`date +/home/pi/Pi0AudioBook/times/%0Hh-%0Mm.mp3`
     ffmpeg -y -i "concat:$tf|$NEWS_FILE" -c copy $ANNOUNCER_FILE_TMP
 fi
 mv -f $ANNOUNCER_FILE_TMP $ANNOUNCER_FILE
